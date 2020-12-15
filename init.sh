87164b861d58acc0001330fa967070c31a4ad891
dongheejeong <donghee950403@gmail.com>
2020-12-16 00:32:14 +0900

    Add init.sh message

--- init.sh
+++ init.sh
@@ -2,11 +2,18 @@
 
 set -e
 
+echo ""
+echo "This script require sudo. (ex. pacman -S, ansible become)"
+echo "If this script takes time too long, sudo may timeout and script may fail."
+echo "If so, execute this script again. It will end in short time because previous execution do things."
+echo ""
+sudo echo "sudo success"
+
 sudo pacman --noconfirm --needed -S ansible
 
 # to use pacman Ansible module
 ansible-galaxy collection install community.general
 
 cd playbook
-ansible-playbook main.yaml
+ansible-playbook main.yaml "$@"
 
